930b262dc7abe342f4dff270258610f319123bdc
ateso96 <andoniteso@gmail.com>
2020-03-09 13:07:21 +0100

    Comentarios de pasos

--- loader.py
+++ loader.py
@@ -1,6 +1,5 @@
 from csv import reader
 
-
 # Metodo para leer los csv con datos
 def load_csv(filename):
     dataset = list()
@@ -11,6 +10,10 @@ def load_csv(filename):
                 continue
             dataset.append(row)
     return dataset
+
+# Crear las features
+
+# Crear los pesos para las instancias de test
 
 filename = 'train.csv'
 dataset = load_csv(filename)
