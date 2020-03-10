5d0948c18218d3e3e3b50fc356fd3c960358a730
ateso96 <andoniteso@gmail.com>
2020-03-10 18:21:07 +0100

    Añadir comentarios

--- loader.py
+++ loader.py
@@ -1,16 +1,19 @@
 import csv
 
+
+# Leer el CSV y separar los datos
 def loadData(file):
-    with open(file, encoding="utf8") as csv_file:
-        reader = csv.reader(csv_file, delimiter = ",", quotechar = '"')
+    with open(file, encoding="utf8") as csvFile:
+        reader = csv.reader(csvFile, delimiter=",", quotechar='"')
         id = []
         text = []
         label = []
         for row in reader:
-            id.append(row[0])
-            text.append(row[1])
-            label.append(row[2])
+            id.append(row[0])  # Añadir los ID
+            text.append(row[1])  # Añadir las preguntas
+            label.append(row[2])  # Indicar la clasificación de cada pregunta
         return id[1:], text[1:], label[1:]
+
 
 id, text, labels = loadData("data/train.csv")
 for pos in range(len(id)):
