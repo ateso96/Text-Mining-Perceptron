85a38cff223c017dfadc6441629e21dbeccc06c6
ateso96 <andoniteso@gmail.com>
2020-03-11 16:43:04 +0100

    Imprimir resultados

--- classifier.py
+++ classifier.py
@@ -1,17 +1,22 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report, multilabel_confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 
 
 def split(data, labels, percent):
-    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=percent, random_state=1)
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=percent, random_state=0)
     return x_train, x_test, y_train, y_test
 
 def classifyMP(x_train, x_test, y_train, y_test):
-    cls = MLPClassifier(solver='lbfgs', alpha=0.00095, learning_rate='adaptive', learning_rate_init=0.005, max_iter=300,
+    cls = MLPClassifier(solver='lbfgs', alpha=0.00095, learning_rate='adaptive', learning_rate_init=0.005, max_iter=1500,
                        random_state=0)
     Perceptron = cls.fit(x_train, y_train)
     predictions = Perceptron.predict(x_test)
     a = accuracy_score(y_test, predictions)
     p = precision_score(y_test, predictions, average='weighted')
     r = recall_score(y_test, predictions, average='weighted')
+    print(a)
+    print(p)
+    print(r)
+    print(multilabel_confusion_matrix(y_test, predictions))
+    print(classification_report(y_test, predictions))
