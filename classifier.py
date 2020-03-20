17ee1cdb85ef379c0551c381f2a8a5637d77041d
ateso96 <andoniteso@gmail.com>
2020-03-20 20:35:04 +0100

    Reducir pruebas de iteracions

--- classifier.py
+++ classifier.py
@@ -17,7 +17,7 @@ def split(data, labels, percent):
 def classifyMP(x_train, x_test, y_train, y_test):
     cls = MLPClassifier()
     parameter_space = {
-        'max_iter': 100 * np.arange(10, 25),
+        'max_iter': [100, 1000, 10000],
         'hidden_layer_sizes': [(100, 10), (200, 20), (300, 30), (400, 40), (500, 50), (600, 60), (700, 70), (800, 80), (900, 90), (1000, 100)],
         'solver': ['lbfgs'],
         'alpha': 10.0 ** -np.arange(1, 5),
