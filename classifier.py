92d76927004f26f25d428439a56fde99a739760b
ateso96 <andoniteso@gmail.com>
2020-03-23 19:33:55 +0100

    Guardar resultados en ficheros

--- classifier.py
+++ classifier.py
@@ -2,9 +2,10 @@ import pickle
 
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report, \
-    multilabel_confusion_matrix
+    multilabel_confusion_matrix, plot_confusion_matrix
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neural_network import MLPClassifier
+import matplotlib.pyplot as plt
 
 
 def split(data, labels, percent):
@@ -31,25 +32,37 @@ def classifyMP(x_train, x_test, y_train, y_test):
     results = "************************************" \
               "\nPERCEPTRON TREC-6 CLASSIFIER" \
               "\n************************************"
-    results += "\n--> Score: ", perceptron.score(x_train, y_train)
-    results += '\n--> Best parameters:\n', perceptron.best_params_, '\n'
+    results += "\n--> Score: " + str(perceptron.score(x_train, y_train))
+    results += '\n--> Best parameters:\n' + str(perceptron.best_params_) + '\n'
 
     predictions = perceptron.predict(x_test)
-    results += "\n--> Accuraccy: ", accuracy_score(y_test, predictions)
-    results += "\n--> Precision: ", precision_score(y_test, predictions, average='weighted')
-    results += "\n--> Recall: ", recall_score(y_test, predictions, average='weighted')
+    results += "\n--> Accuraccy: " + str(accuracy_score(y_test, predictions))
+    results += "\n--> Precision: " + str(precision_score(y_test, predictions, average='weighted'))
+    results += "\n--> Recall: " + str(precision_score(y_test, predictions, average='weighted'))
 
-    results += "\n--> Classification Report: \n",classification_report(y_test, predictions)
+    results += "\n--> Classification Report: \n" + classification_report(y_test, predictions)
 
-    results += "\n--> Confusion Matrix: \n",multilabel_confusion_matrix(y_test, predictions)
+    titles_options = [("Confusion matrix, without normalization", None),
+                      ("Normalized confusion matrix", 'true')]
+    for title, normalize in titles_options:
+        disp = plot_confusion_matrix(perceptron, x_test, y_test,
+                                     display_labels=['ABBR', 'DESC', 'ENTY', 'HUM', 'LOC', 'HUM'],
+                                     cmap=plt.cm.Blues)
+        disp.ax_.set_title(title)
 
-    with open('resultados.txt', 'wb') as picklefile:
+        print(title)
+        print(disp.confusion_matrix)
+
+    plt.savefig('results/matrix.png')
+
+    with open('results/resultados.txt', 'wb') as picklefile:
         pickle.dump(results, picklefile)
 
-    with open('modeloPerceptron', 'wb') as picklefile:
+    with open('results/modeloPerceptron', 'wb') as picklefile:
         pickle.dump(perceptron, picklefile)
 
+
 def makePredictions(data):
-    with open('modeloPerceptron', 'rb') as training_model:
+    with open('results/modeloPerceptron', 'rb') as training_model:
         model = pickle.load(training_model)
     return model.predict(data)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from getRAW import loadData
 from preprocessing import *
 from classifier import split, classifyMP, makePredictions
 
-file = "data/train_small.csv"
+file = "data/train.csv"
 filePreds = "data/test_unk.csv"
 id, text, labels = loadData(file)
 idPreds, textPreds, labelsPreds = loadData(filePreds)
@@ -11,13 +11,13 @@ idPreds, textPreds, labelsPreds = loadData(filePreds)
 data = rawToVector(text)
 dictionary = getDictionary(data)
 
-with open('dictionary', 'wb') as picklefile:
+with open('results/dictionary', 'wb') as picklefile:
     pickle.dump(dictionary, picklefile)
 
 data = tfidf(data, dictionary)
 
 x_train, x_test, y_train, y_test = split(data, labels, 0.3)
-#classifyMP(x_train, x_test, y_train, y_test)
+classifyMP(x_train, x_test, y_train, y_test)
 
 textPredsAux = textPreds.copy()
 dataPreds = rawToVector(textPreds)
