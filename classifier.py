f97d960f77d7372d4ec4f59021e465290a2ef05e
ateso96 <andoniteso@gmail.com>
2020-03-27 16:05:46 +0100

    Ajustar barrido

--- classifier.py
+++ classifier.py
@@ -9,25 +9,31 @@ import matplotlib.pyplot as plt
 
 
 def split(data, labels, percent):
-    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=percent, random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=percent, random_state=42)
     return x_train, x_test, y_train, y_test
 
 
 def classifyMP(x_train, x_test, y_train, y_test):
-    cls = MLPClassifier()
+    cls = MLPClassifier(random_state=1)
     parameter_space = {
-        'max_iter': [1000, 2000],
-        'hidden_layer_sizes': [(550, 55), (600, 60), (650, 65)],
+        'max_iter': [1500, 1600, 1700, 1800, 1900, 2000],
+        'hidden_layer_sizes': [(3, 1), (5, 2), (9, 4)],
+        'solver': ['lbfgs', 'sgf', 'adam'],
+        'alpha': [1e-5, 3e-5, 1e-4, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2]
     }
 
-    perceptron = GridSearchCV(cls, parameter_space, n_jobs=-1, scoring='f1_weighted')
+    perceptron = GridSearchCV(cls, parameter_space, n_jobs=-1, cv=10, scoring='f1_weighted')
     perceptron.fit(x_train, y_train)
-
+    bestParameters = perceptron.best_params_
     results = "************************************" \
               "\nPERCEPTRON TREC-6 CLASSIFIER" \
               "\n************************************"
-    results += "\n--> Score: " + str(perceptron.score(x_train, y_train))
+    results += "\n--> Best F1 Score: " + str(perceptron.best_score_)
     results += '\n--> Best parameters:\n' + str(perceptron.best_params_) + '\n'
+
+    perceptron = MLPClassifier(max_iter=['max_iter'], solver=bestParameters['solver'], alpha=bestParameters['alpha'],
+                               hidden_layer_sizes=bestParameters['hidden_layer_sizes'], random_state=1)
+    perceptron.fit(x_train, y_train)
 
     predictions = perceptron.predict(x_test)
     results += "\n--> Accuraccy: " + str(accuracy_score(y_test, predictions))
